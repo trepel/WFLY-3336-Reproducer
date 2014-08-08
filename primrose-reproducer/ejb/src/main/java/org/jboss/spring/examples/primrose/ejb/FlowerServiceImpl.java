b8e5ccf172630844b8fa985df772d7574bf99dc5
Tomas Repel <trepel@redhat.com>
2014-08-08 16:35:40 +0200

    Added SpringBeanAutowiringSupport to EJB

--- primrose-reproducer/ejb/src/main/java/org/jboss/spring/examples/primrose/ejb/FlowerServiceImpl.java
+++ primrose-reproducer/ejb/src/main/java/org/jboss/spring/examples/primrose/ejb/FlowerServiceImpl.java
@@ -10,11 +10,12 @@ import org.jboss.spring.examples.primrose.dao.FlowerDao;
 import org.jboss.spring.examples.primrose.entity.Flower;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 @Stateless
 @Local(FlowerService.class)
 @Interceptors(SpringBeanAutowiringInterceptor.class)
-public class FlowerServiceImpl implements FlowerService
+public class FlowerServiceImpl extends SpringBeanAutowiringSupport implements FlowerService
 {
    @Autowired
    private FlowerDao flowerDao;
